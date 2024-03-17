57608e02c20c754528b02d6a3e8a43903345c65b
Andrés Pérez Cano <aperezcan@unal.edu.co>
2024-03-17 02:33:48 -0500

    correccion animal

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -19,6 +19,34 @@ class Animal:
             string += f", la zona en la que me ubico es {self._zona}, en el {self._zoo}"
         return string
 
+    def setNombre(self,nombre):
+        self._nombre = nombre
+    def getNombre(self):
+        return self._nombre
+    def setEdad(self,edad):
+        self._edad = edad
+    def getEdad(self):
+        return self._edad
+    def setHabitat(self,habitat):
+        self._habitat = habitat
+    def getHabitat(self):
+        return self._habitat
+    def setGenero(self,genero):
+        self._genero = genero
+    def getGenero(self):
+        return self._genero
+    def setZona(self,zona):
+        self.zona = zona
+    def getZona(self):
+        return self._zona
+    @classmethod
+    def setTotalAnimales(cls,totalAnimales):
+        cls._totalAnimales = totalAnimales
+    @classmethod 
+    def getTotalAnimales(cls):
+        return cls._totalAnimales
+
+
     
     
 
